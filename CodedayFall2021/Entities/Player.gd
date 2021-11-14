1408fcb99562263d3b320d4ed6c191f7f08e050b
SkyanSam <samuelwiggins15@gmail.com>
2021-11-14 07:57:20 -0500

    edits to hair blower effect?

--- CodedayFall2021/Entities/Player.gd
+++ CodedayFall2021/Entities/Player.gd
@@ -7,6 +7,7 @@ extends KinematicBody
 var velocity = Vector3()
 var degree_margin = 30
 var last_velocity = Vector3.ONE
+export var speed = 100
 # Called when the node enters the scene tree for the first time.
 func _ready():#setup
 	pass # Replace with function body.
@@ -20,13 +21,13 @@ func _process(delta): #void/loop
 	
 	
 	if Input.get_action_strength("s"):
-		velocity.z = 100*delta
+		velocity.z = speed*delta
 	if Input.get_action_strength("a"):
-		velocity.x = -100*delta
+		velocity.x = -speed*delta
 	if Input.get_action_strength("w"):
-		velocity.z = -100*delta
+		velocity.z = -speed*delta
 	if Input.get_action_strength("d"):
-		velocity.x = 100*delta
+		velocity.x = speed*delta
 	#if Input.get_action_strength("ui_select") and $RayCast.is_colliding():
 			#velocity.y = 250*delta
 # warning-ignore:return_value_discarded
@@ -35,11 +36,15 @@ func _process(delta): #void/loop
 		last_velocity = velocity
 	
 	if (Input.get_action_strength("hair_blow")):
-		
+		var vect = Vector2()
+		vect.angle()
 		for e in get_tree().get_nodes_in_group("Enemies"):
 			var diff = (e.translation - translation).normalized()
-			var enemy_dir_angle = rad2deg(atan2(diff.x, diff.y))
-			var player_dir_angle = rad2deg(atan2(last_velocity.normalized().x, last_velocity.normalized().y))
+			var diff2d = Vector2(diff.x, diff.z)
+			var enemy_dir_angle = rad2deg(diff2d.angle()) + 180
+			var last_velocity2d = Vector2(last_velocity.x, last_velocity.z)
+			var player_dir_angle = rad2deg(last_velocity2d.normalized().angle())
+			print("Hair dry with e" + str(enemy_dir_angle) + " and p" + str(player_dir_angle))
 			if (abs(enemy_dir_angle - player_dir_angle) < degree_margin):
 				e.player_push(-last_velocity)
 	# UH yeah hairblower doesnt WORK?!?!?
