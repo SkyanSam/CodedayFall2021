5760725f17b09030be80dc4bf13eb2c54db9c349
SkyanSam <samuelwiggins15@gmail.com>
2021-11-13 23:57:46 -0500

    Cubemap

--- CodedayFall2021/Block/Block.gd
+++ CodedayFall2021/Block/Block.gd
@@ -16,9 +16,12 @@ func _ready():
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
-	var left_mat = material.duplicate()
-	left_mat.TEXTURE_ALBEDO = left_wall_texture
-	var right_mat = material.duplicate()
-	right_mat.TEXTURE_ALBEDO = right_wall_texture
-	var top_mat = material.duplicate()
-	top_mat.TEXTURE_ALBEDO = top_wall_texture
+	var left_mat = SpatialMaterial.new()
+	left_mat.albedo_texture = left_wall_texture
+	$LeftFace.material_override = left_mat
+	var right_mat = SpatialMaterial.new() 
+	right_mat.albedo_texture = right_wall_texture
+	$RightFace.material_override = right_mat
+	var top_mat = SpatialMaterial.new()
+	top_mat.albedo_texture = top_wall_texture
+	$TopFace.material_override = top_mat
--- CodedayFall2021/Tilemap/Tilemap.gd
+++ CodedayFall2021/Tilemap/Tilemap.gd
@@ -1,31 +1,50 @@
 extends Node
 
-export var tilemap_csv_filename = "res://Tilemap/tilemap.csv" #= preload("res://Tilemap/tilemap.csv")
+export var tilemap_csv_filename = "res://Tilemap/tilemap.txt" #= preload("res://Tilemap/tilemap.csv")
 export var blocks = []
+export var preloaded_blocks = []
 # Called when the node enters the scene tree for the first time.
 func _ready():
+	for b in blocks:
+		preloaded_blocks.append(load(b))
 	var result = []
 	var file = File.new()
-	if file.open(tilemap_csv_filename, file.READ) != 0:
-		print("opened file")
-		file.get_line()
-		#get_csv_line ( String delim=”,” ) 
-		while not file.eof_reached():
-			var line = file.get_line()
-			result.append(line)
-		file.close()
-	var array_3d = []
-	for r in result:
-		array_3d.append(r.split(','))
+	file.open(tilemap_csv_filename, file.READ)
+	print("opened file")
+	#get_csv_line ( String delim=”,” ) 
+	while not file.eof_reached():
+		var line = file.get_line()
+		result.append(line)
+	file.close()
 	
-	for y in result:
-		for x in y:
-			if (array_3d[y][x] >= 0):
-				var block = blocks[array_3d[y][x]].instance()
-				block.global_position = Vector3(x, 0, y)
-					
+	var height = result.size()
+	var width = result[0].split(',').size()
+	var matrix = create_map(height , width)
+	
+	for y in range(result.size()):
+		var list = result[y].split(',')
+		for x in range(list.size()):
+			matrix[y][x] = int(list[x])
+
+	for y in height:
+		for x in width:
+			if (int(matrix[y][x]) >= 0):
+				var block : Spatial = preloaded_blocks[matrix[y][x]].instance()
+				block.translation = Vector3(x, 0, y)
+				add_child(block)
+	print(str(result.size()) + "," + str(result[0].split(',').size()))
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 #func _process(delta):
 #	pass
+
+func create_map(w, h):
+	var map = []
+
+	for x in range(w):
+		var col = []
+		col.resize(h)
+		map.append(col)
+
+	return map
